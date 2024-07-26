7962829d01b47ebf642a58ee349b1af932812e0e
Marcin Chowaniec <marcin.chowaniec@outlook.com>
2024-07-26 07:49:23 +0200

    Project init

---
+++ projects_sm/src/main/java/com/marcincho/projects_sm/ProjectsSmApplication.java
@@ -0,0 +1,13 @@
+package com.marcincho.projects_sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsSmApplication.class, args);
+	}
+
+}
---
+++ projects_sm/src/test/java/com/marcincho/projects_sm/ProjectsSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcincho.projects_sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
